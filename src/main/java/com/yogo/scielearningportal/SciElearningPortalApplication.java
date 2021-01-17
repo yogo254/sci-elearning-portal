8d541c0e0ba2473e66073df35320eb8182a2c814
yogo254 <yogo254@yahoo.com>
2021-01-17 18:52:45 +0300

    initial commit

---
+++ src/main/java/com/yogo/scielearningportal/SciElearningPortalApplication.java
@@ -0,0 +1,13 @@
+package com.yogo.scielearningportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SciElearningPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SciElearningPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogo/scielearningportal/SciElearningPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogo.scielearningportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SciElearningPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
